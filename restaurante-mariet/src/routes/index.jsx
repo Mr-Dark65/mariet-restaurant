fce3422e91cc7176a857269b775b8c9ebad15b8e
Mr-Dark65 <steevenloor2412@gmail.com>
2025-06-09 12:25:54 -0500

    Implementacion de hash route

--- restaurante-mariet/src/routes/index.jsx
+++ restaurante-mariet/src/routes/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { createBrowserRouter } from 'react-router-dom';
+import { createHashRouter } from 'react-router-dom';
 import RootLayout from '../layouts/RootLayout';
 import Home from '../pages/Home';
 import Nosotros from '../features/Nosotros/nosotros';
 import ContactHeader from '../features/Contact-Header/contactheader';
 import Menu from '../features/Menu/menu';
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   {
     path: '/',
     element: <RootLayout />,

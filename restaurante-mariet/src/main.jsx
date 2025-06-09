6011279eae73a890129ab47e65daef238f5158fa
Mr-Dark65 <steevenloor2412@gmail.com>
2025-06-09 12:36:16 -0500

    Implementacion 4.0

--- restaurante-mariet/src/main.jsx
+++ restaurante-mariet/src/main.jsx
@@ -1,7 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './routes'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
--- restaurante-mariet/src/routes/index.jsx
+++ restaurante-mariet/src/routes/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { createHashRouter } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 import RootLayout from '../layouts/RootLayout';
 import Home from '../pages/Home';
 import Nosotros from '../features/Nosotros/nosotros';
 import ContactHeader from '../features/Contact-Header/contactheader';
 import Menu from '../features/Menu/menu';
 
-export const router = createHashRouter([
+export const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
